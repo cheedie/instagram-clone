ee5f5a44e74670ff3e131f87560b398dcd04be6b
cheedieebeogu <cheedie.ebeogu@gmail.com>
2022-03-08 06:45:33 +0100

    template

---
+++ src/components/BottomNavbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BottomNavbar = () => {
+  return <div>this is bottom nav bar</div>;
+};
+
+export default BottomNavbar;
---
+++ src/components/TopNavBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopNavBar = () => {
+  return <div>this is top nav bar</div>;
+};
+
+export default TopNavBar;
