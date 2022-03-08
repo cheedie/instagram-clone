ea79757cc17b25f64e364c55de0c0ec69ae0e3a2
cheedieebeogu <cheedie.ebeogu@gmail.com>
2022-03-08 19:40:19 +0100

    top nav bar and create a post component

---
+++ src/assets/topNavAssets/index.js
@@ -0,0 +1,19 @@
+import { ReactComponent as HomeIcon } from "./home.svg";
+import { ReactComponent as AddPostIcon } from "./add-post.svg";
+import { ReactComponent as LikeIcon } from "./like.svg";
+import { ReactComponent as MessageIcon } from "./message.svg";
+import { ReactComponent as AntIcon } from "./ant.svg";
+import { ReactComponent as SearchIcon } from "./search-icon.svg";
+import { ReactComponent as Drag } from "./drag.svg";
+import { ReactComponent as Drop } from "./drop.svg";
+
+export {
+  HomeIcon,
+  AddPostIcon,
+  LikeIcon,
+  MessageIcon,
+  AntIcon,
+  SearchIcon,
+  Drag,
+  Drop,
+};
