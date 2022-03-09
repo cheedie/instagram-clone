b25c9bf079adac07e7af47e6190ca518c3b3e4f7
teelda <matildaipeh@gmail.com>
2022-03-09 09:06:29 +0100

    updated the login page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Profile from "./pages/Profile";
 import SplashScreen from "./pages/SplashScreen";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
- 
+
 function App() {
   return (
     <BrowserRouter>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,64 @@
 import React from "react";
-import { FaFacebook } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { StyledFormWrapper } from "../components/Styles/Form.Styled";
+import { GlobalStyle } from "../components/Styles/Form.Styled";
+import { StyledForm } from "../components/Styles/Form.Styled";
+import { StyledButton } from "../components/Styles/Form.Styled";
+import { StyledWrapper } from "../components/Styles/Form.Styled"
+import { StyledText } from "../components/Styles/Form.Styled";
+import { Container } from "../components/Styles/Form.Styled";
+import { StyledDownload } from "../components/Styles/Form.Styled";
+import login_logo from "../assets/Instagram-Wordmark.svg";
+import img3 from "../assets/downloadios.svg";
+import img2 from "../assets/downloadgoogle.svg";
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <> 
+      <> 
         <GlobalStyle />
-        <StyledFormWrapper>
-            <StyledForm>
-                <img src="../assets/Login_Logo.svg" alt="Instagram logo" />
-                <input type="email or number" placeholder="Phone number or Email address" />
-                <input type="text" placeholder="Full name" />
-                <input type="text" placeholder="Username" />
+        <StyledWrapper>
+          <Container>
+            <StyledFormWrapper>
+              <img src={login_logo} alt="Instagram logo" />
+              <StyledForm onSubmit={handleSubmit}>
+                <input 
+                  type="email or number" 
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  placeholder="Phone number or Username or Email address" />
                 <input type="password" placeholder="Password" />
-                <StyledButton>Sign up</StyledButton>
+                <StyledButton value="Submit" >Login</StyledButton>
                 <StyledText>Forgot your login details? Get help logging in.</StyledText>
                 <h3><span>Or</span></h3>
-                <FaFacebook />
                 <StyledButton>Continue with Facebook</StyledButton>
-                <StyledText>Dont have an account?  Sign up.</StyledText>
-            </StyledForm>
-        </StyledFormWrapper>
-    </>
+                <StyledText>Dont have an account? 
+                  <Link to="/signup" >
+                    <span> Sign up.</span>
+                  </Link> 
+                </StyledText>
+              </StyledForm>
+              <StyledDownload>
+                <Link to='/'>
+                    <img src={img3} alt="download app" />
+                </Link>
+                <Link to='/'>
+                    <img src={img2} alt= "download app" />
+                </Link>
+              </StyledDownload>
+            </StyledFormWrapper>
+          </Container>
+        </StyledWrapper>
+      </>
   )
-}
+};
 
 
 export default Login;
