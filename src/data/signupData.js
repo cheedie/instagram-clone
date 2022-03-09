5ff2daeee29734a3c1072654088b3aa9cf6f95e8
cheedie <75317125+cheedie@users.noreply.github.com>
2022-03-09 21:01:28 +0100

    Revert "Revert "Login page ""

---
+++ src/data/signupData.js
@@ -0,0 +1,6 @@
+export const signUpData = [
+    {name:"email", type:"text", stateFunc : "setEmail"},
+    {name:"fullname", type:"text",},
+    {name:"username", type:"text",},
+    {name:"password", type:"text",},
+]
